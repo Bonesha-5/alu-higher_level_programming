1e2bb29f52e90bd0a648835a82a8b8aac153b312
Bonesha-5 <u.kevine@alustudent.com>
2025-07-08 06:58:49 -0700

    add file

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import sys
-if __name__ == "__name__":
+if __name__ == "__main__":
     args_count = sys.argv[1:]
     total = sum(int(arg) for arg in args_cont)
     print(total)
