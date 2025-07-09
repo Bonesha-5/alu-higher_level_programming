b2b7ac58ef938776f14279a4b1eb26646735cbb4
Bonesha <u.kevine@alustudent.com>
2025-07-09 13:29:31 +0200

    updated

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if idx < 0 or idx >= len(my_list):
-        return my_list
-    my_list[idx] = element
+    (idx >= 0 and idx < len(my_list)) and my_list.__setitem__(idx, element)
     return my_list
