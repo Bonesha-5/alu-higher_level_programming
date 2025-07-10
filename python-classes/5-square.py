16a270471c7a948b1418505d9530c6ffe1b18382
Bonesha-5 <u.kevine@alustudent.com>
2025-07-10 10:24:48 -0700

    p

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 """Defines a Square class with printed representation."""
+
+
 class Square:
     """Square class with size property, area calculation, and print method."""
     def __init__(self, size=0):
         self.size = size
+
     @property
     def size(self):
         return self.__size
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -14,8 +18,10 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
     def area(self):
         return self.__size * self.__size
+
     def my_print(self):
         if self.__size == 0:
             print()
