ad7038b6913308f0565dc712e6bca12d02b75f60
Bonesha-5 <u.kevine@alustudent.com>
2025-07-10 10:29:07 -0700

    3

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Defines a Square class with a private size."""
+
+
 class Square:
     """Square class with a private instance attribute size."""
     def __init__(self, size):
